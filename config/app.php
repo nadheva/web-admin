b60a79bd014a81805ccedc3578eb142c5f2894fb
nadheva17 <nadheva17@student.uns.ac.id>
2021-10-19 09:33:31 +0700

    update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://web-kg2.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
